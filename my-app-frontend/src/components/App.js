fb9ba2134b7eab5fa628e0c99ace0ae2859716ec
Axel Aquino <aquino.axel@gmail.com>
2022-03-31 16:18:25 -0400

    login

--- my-app-frontend/src/components/App.js
+++ my-app-frontend/src/components/App.js
@@ -1,80 +1,77 @@
-
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Nav from './NavBar/Nav';
-import Home from './Home';
-import YourFavs from './YourFavs';
-import Combiner from './AlbumAdd/Combiner';
-import Discover from './Discover';
-import Login from './NavBar/Login';
-
+import Nav from "./NavBar/Nav";
+import Home from "./Home";
+import YourFavs from "./YourFavs";
+import Combiner from "./AlbumAdd/Combiner";
+import Discover from "./Discover";
+import Login from "./NavBar/Login";
 
 function App() {
-    const [page, setPage] = useState("/")
-    const [likedList, setLikedList] = useState([])
-    const [search, setSearch] = useState(" ")
+  const [page, setPage] = useState("/");
+  const [likedList, setLikedList] = useState([]);
+  const [search, setSearch] = useState(" ");
 
-    function onSearchChange(input){
-        setSearch(input)
-        console.log(search)
+  function onSearchChange(input) {
+    setSearch(input);
+    console.log(search);
+  }
+
+  function handleAddAlbum(album) {
+    const foundIndex = likedList.findIndex((item) => album.name === item.name);
+    if (foundIndex === -1) {
+      setLikedList([...likedList, album]);
+      fetch("http://localhost:9292/albums", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(album),
+      })
+        .then((response) => response.json())
+        .catch((error) => console.error("Error:", error));
+    } else {
+      console.log("Album already on list");
     }
+  }
 
-    function handleAddAlbum(album){
-        const foundIndex = likedList.findIndex(item => album.name === item.name);
-        if (foundIndex === -1) {
-            setLikedList([...likedList, album]);
-            fetch("http://localhost:9292/albums", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(album),
-            })
-            .then(response => response.json())
-            .catch(error => console.error("Error:", error))
-        } else {
-            console.log("Album already on list")
-        }
+  function getUserAlbums(album) {
+    if (likedList.find((item) => item === album)) {
+      const newList = likedList.filter((item) => item !== album);
+      setLikedList(newList);
+      fetch("http://localhost:3000/" + album.id, {
+        method: "DELETE",
+      });
+    } else {
+      console.log("Not found");
     }
+  }
 
-    function getUserAlbums (album){
-        if(likedList.find(item => item === album)) {
-            const newList = likedList.filter(item => item !== album)
-            setLikedList(newList)
-            fetch("http://localhost:3000/"+album.id, {
-            method: "DELETE" })
-        } else {
-            console.log("Not found")
-        }
-    }
-
-
-
-    return(
-        <BrowserRouter>
-            <Nav onChangePage = {setPage} className="nav" />
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route exact path ="/AlbumPicker">
-                    <Combiner search = {search} onChange ={onSearchChange}/>
-                </Route>
-                <Route path="/YourFavs">
-                    <YourFavs/>
-                </Route>
-                <Route path="/Discover">
-                    <Discover/>
-                </Route>
-                <Route path="/Login">
-                    <Login/>
-                </Route>
-                <Route path="*">
-                    <h1>404 not found</h1>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Nav onChangePage={setPage} className="nav" />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/AlbumPicker">
+          <Combiner search={search} onChange={onSearchChange} />
+        </Route>
+        <Route path="/YourFavs">
+          <YourFavs />
+        </Route>
+        <Route path="/Discover">
+          <Discover />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <h1>404 not found</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- my-app-frontend/src/components/NavBar/Login.js
+++ my-app-frontend/src/components/NavBar/Login.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Login(){
+function Login() {
+  const [values, setValues] = useState({ username: "", password: "" });
 
+  const handleUserNameInputChange = (event) => {
+    // console.log("writing");
+    setValues({ ...values, username: event.target.value });
+  };
+
+  const handlePasswordInputChange = (event) => {
+    // console.log("pass");
+    setValues({ ...values, password: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = {
+      user_name: values.username,
+      password: values.password,
+    };
+    fetch("http://localhost:9292/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((jsonData) => console.log("data", jsonData));
+    console.log("i submit ");
+  };
+
+  const handleDelete = (event) => {};
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>User Name:</label>
+        <input
+          onChange={handleUserNameInputChange}
+          value={values.username}
+          type="text"
+          placeholder="username"
+        ></input>
+        <br />
+        <label>Password:</label>
+        <input
+          onChange={handlePasswordInputChange}
+          value={values.password}
+          type="password"
+          placeholder="password"
+        ></input>
+        <br />
+        <button onClick={handleSubmit}>Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
+
+// // create new user
+// fetch('http://localhost:9292/users', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+// 	user_name: "test",
+// 	password: "test"
+// }),
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Success:', data);
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
