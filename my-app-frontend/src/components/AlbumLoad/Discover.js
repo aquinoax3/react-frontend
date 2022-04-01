2d7adf8ffb1454992142e0b46d671c8c80d52fd7
Axel Aquino <aquino.axel@gmail.com>
2022-04-01 12:21:03 -0400

    Discover

--- my-app-frontend/src/components/AlbumLoad/Discover.js
+++ my-app-frontend/src/components/AlbumLoad/Discover.js
@@ -1,36 +1,41 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import DiscCard from "./DiscCard";
 import Card from "../AlbumAdd/Card";
 
 function Discover() {
   const [userAlbums, setUserAlbums] = useState([]);
+  const [input, setInput] = useState("");
 
-  function handleSearchInput() {
+  function handleSearchInput(e) {
+    e.preventDefault();
     console.log("searched");
-    fetch(`http://localhost:9292/toplists`)
+    fetch(`http://localhost:9292/users/${input}/albums`)
       .then((response) => response.json())
       .then((jsonData) => setUserAlbums(jsonData));
   }
-
 
   let showenItems = userAlbums.map((album) => {
     return <DiscCard key={album.id} album={album} />;
   });
   return (
-    <div id="content_container">
-      <p className="search_content">
-        Search your friend's username to see their album choices.
-      </p>
-      <input
-        onChange={handleSearchInput}
-        className="search_content"
-        type="text"
-        name="search"
-        placeholder="Search..."
-      ></input>
+    <>
+      <div id="content_container">
+        <p className="search_content">
+          Search your friend's username to see their album choices.
+        </p>
+        <form onSubmit={(e) => handleSearchInput(e)}>
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            className="search_content"
+            type="text"
+            name="search"
+            placeholder="Search..."
+            value={input}
+          ></input>
+        </form>
+      </div>
       {showenItems}
-    </div>
+    </>
   );
 }
 
